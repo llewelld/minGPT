f15a482b403d1b4462782b1f0b1255abf27ba727
David Llewellyn-Jones <dllewellyn-jones@turing.ac.uk>
2023-05-11 10:14:08 +0100

    Save vocabulary to checkpoint
    This allows the vocabulary to be reused after loading the checkpoint
    without having to reload the data in order to reconstruct the
    vocabulary.

--- train.py
+++ train.py
@@ -30,11 +30,13 @@ class CharDataset(Dataset):
         data_size, vocab_size = len(data), len(chars)
         rank_zero_info('data has %d characters, %d unique.' % (data_size, vocab_size))
 
+        assert vocab_size < 256, 'The vocabulary exceeds byte-size storage'
+
         self.stoi = {ch: i for i, ch in enumerate(chars)}
         self.itos = {i: ch for i, ch in enumerate(chars)}
         self.block_size = block_size
         self.vocab_size = vocab_size
-        self.data = data
+        self.data = bytes([self.stoi[s] for s in data])
 
     def __len__(self):
         return math.ceil(len(self.data) / (self.block_size + 1))
@@ -42,12 +44,10 @@ class CharDataset(Dataset):
     def __getitem__(self, idx):
         # we're actually going to "cheat" and pick a spot in the dataset at random
         i = np.random.randint(0, len(self.data) - (self.block_size + 1))
-        chunk = self.data[i:i + self.block_size + 1]
-        dix = [self.stoi[s] for s in chunk]
+        dix = list(self.data[i:i + self.block_size + 1])
         x = torch.tensor(dix[:-1], dtype=torch.long)
         y = torch.tensor(dix[1:], dtype=torch.long)
         return x, y
-
 
 class GPT(pl.LightningModule):
     def __init__(self,
@@ -61,7 +61,8 @@ class GPT(pl.LightningModule):
                  n_layer=12,
                  n_head=4,
                  resid_pdrop=0.1,
-                 attn_pdrop=0.1
+                 attn_pdrop=0.1,
+                 itos=[],
                  ):
         super().__init__()
         self.save_hyperparameters()
@@ -77,6 +78,10 @@ class GPT(pl.LightningModule):
         self.block_size = self.hparams.block_size
 
         self.blocks = nn.ModuleList([Block(self.hparams) for _ in range(self.hparams.n_layer)])
+
+        # Retain a copy of the vocabulary conversion map for checkpointing purposes
+        self.itos = itos
+        self.init_stoi()
 
     def configure_optimizers(self):
         no_decay = ["bias", "LayerNorm.weight"]
@@ -121,6 +126,20 @@ class GPT(pl.LightningModule):
         self.log('train_loss', loss)
         return loss
 
+    def on_save_checkpoint(self, checkpoint):
+        # Save out the vocab conversion dictionary to the checkpoint
+        # This avoids us having to load in the data to use the trained model
+        checkpoint['itos'] = self.itos
+
+    def on_load_checkpoint(self, checkpoint):
+        # Load in the vocab conversion dictionary from the checkpoint
+        # This avoids us having to load in the data to use the trained model
+        self.itos = checkpoint['itos']
+        self.init_stoi()
+
+    def init_stoi(self):
+        # Create a reverse lookup, strings to integers
+        self.stoi = {value: key for key, value in self.itos.items()}
 
 if __name__ == '__main__':
     seed_everything(42)
@@ -150,7 +169,8 @@ if __name__ == '__main__':
             n_layer=args.n_layer,
             n_head=args.n_head,
             n_embd=args.n_embd,
-            learning_rate=args.learning_rate
+            learning_rate=args.learning_rate,
+            itos=train_dataset.itos,
         )
 
     lr_decay = LearningRateDecayCallback(
